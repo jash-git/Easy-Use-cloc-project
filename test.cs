4bcde0009ec7ec64849c3b8f5f491067daee79dc
jash-git <jash.liao@gmail.com>
2017-09-19 08:39:57 +0800

    test.cs

---
+++ test.cs
@@ -0,0 +1,40 @@
+﻿/*
+使用cloc 計算程式行數，程式碼內的空白列和註解都會被自動替除
+
+但是因為它非編譯器，所以不會替除無用#if內容
+
+因此該範例會偵測到31行
+*/
+
+#define DEBUG
+#define MYTEST 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ifdefined
+{
+    class Program
+    {
+        static void pause()
+        {
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+        static void Main(string[] args)
+        {
+            #if (DEBUG && !MYTEST)
+				Console.WriteLine("DEBUG is defined");  
+            #elif (!DEBUG && MYTEST)
+				Console.WriteLine("MYTEST is defined");  
+            #elif (DEBUG && MYTEST)
+				Console.WriteLine("DEBUG and MYTEST are defined");
+            #else
+				Console.WriteLine("DEBUG and MYTEST are not defined");  
+            #endif
+            pause();
+        }
+    }
+}
